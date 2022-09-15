ce23f3753e8f729b5f5e3581d625872a92cf9226
Natanael Machado <natanaelsm0102@gmail.com>
2022-09-15 10:47:58 -0300

    update

---
+++ listav.py
@@ -0,0 +1,34 @@
+#Exercício A:
+
+x=2
+y=5
+if y>8:
+    y=y*2
+else:
+    x=x*2
+print (x+2)
+
+#Exercício B:
+
+a=0
+for i in range (1, 10):
+    if c!=3:
+        for j in range (1, 7):
+            print ('oi')
+        a= a+1
+print (f'A palavra oi foi imprimida {a*6} vezes!')
+
+
+#Exercício C:
+
+a=0
+for c in range (1067, 3628):
+    if c%2==0:
+        if c%7==0:
+            a=a+1
+    
+print (f' A resposta é {a}!')
+
+#Exercício D:
+
+
