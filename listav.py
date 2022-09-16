3736008d5ffca760e43cabae5d00f408f0d62f63
Natanael Machado <natanaelsm0102@gmail.com>
2022-09-16 10:47:46 -0300

    update 2
    falta apenas a questão E

--- listav.py
+++ listav.py
@@ -31,4 +31,10 @@ print (f' A resposta é {a}!')
 
 #Exercício D:
 
+cont = 0
+for c in range(18644, 33088):
+    if '2' in str(c) and '7' not in str(c):
+        cont = cont + 1
+print (cont)
 
+#Exercício E:
